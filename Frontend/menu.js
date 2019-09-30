a62a96eecd6b1fb4d608700b7c274c8706a8b9e2
dblitz <blitzy2k@gmail.com>
2019-09-30 13:17:23 -0400

    Employee table creation updated

--- Frontend/menu.js
+++ Frontend/menu.js
@@ -4,6 +4,7 @@
 // const EMPLOYEE_URL = ``
 let reimbursementtable = document.getElementById("employeereimbursement");
 let vr = document.getElementById("viewreimbursement");
+let expenselist = document.getElementById("ereimbursementlist");
 
 let olddisplay = reimbursementtable.style.display;
 reimbursementtable.style.display = "none";
@@ -12,7 +13,30 @@ vr.addEventListener("click", (event) => {
     event.stopPropagation();
     reimbursementtable.style.display = olddisplay;
     /*
-    fetch().then.catch();
+    fetch().then().catch();
     */
+   expenselist.innerHTML = `
+   <tr>
+        <td>
+            <input type = "radio" name="expense"> Expense Id</input>
+        </td>
+        <td>
+        amount
+        </td>
+        <td>
+        type
+        </td>
+        <td>
+        description
+        </td>
+        <td>
+        date
+        </td>
+        <td>
+        status
+        </td>
+   </tr> 
+
+    `;
 });
 

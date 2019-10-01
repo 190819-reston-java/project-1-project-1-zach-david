9e20f00180a5e7b0b15c8b68a1997af5a4bfe638
dblitz <blitzy2k@gmail.com>
2019-10-01 00:21:28 -0400

    Some more JS

---
+++ src/main/WebApp/login.js
@@ -0,0 +1,10 @@
+'use strict'
+
+let login = false;
+
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+
+let login = document.getAnimations("login");
+
+login.addEventListener("click", event)
---
+++ src/main/WebApp/menu.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const BASE_URL = "/Dashpay"
+const EXPENSE_URL = `${BASE_URL}/expenses`
+
+let expenseDisplay = document.getElementById("expenseDisplay");
+let reimbursementtable = document.getElementById("employeereimbursement");
+let vr = document.getElementById("viewreimbursement");
+ 
+
+let expenselist = document.getElementById("ereimbursementlist");
+
+let olddisplay = reimbursementtable.style.display;
+reimbursementtable.style.display = "none";
+
+vr.addEventListener("click", (event) => {
+    event.stopPropagation();
+    reimbursementtable.style.display = olddisplay;
+    
+    fetch(EXPENSE_URL, { method: "GET" })
+        .then((expenseJson)=>{
+        clearDisplay();
+        for(let expense in expenseJson) {
+            //console.log(expense)
+            createLi(expense);
+        }
+    }).catch();
+    
+  
+   
+});
+
+let clearDisplay = () => {
+    expenseDisplay.innerHTML = "";
+}
+
+let createLi = (expense) => {
+    let li = document.createElement("li");
+    li.innerText =  `${expense.expenseid}`
+    expenseDisplay.appendLi();   
+}
